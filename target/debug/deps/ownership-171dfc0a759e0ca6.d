58a40688ff6bc246d1229343aa80fd4aa05dfc88
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-04-18 12:05:18 -0500

    first commit

---
+++ target/debug/deps/ownership-171dfc0a759e0ca6.d
@@ -0,0 +1,5 @@
+/Users/admin/Documents/rust/ownership/ownership/target/debug/deps/libownership-171dfc0a759e0ca6.rmeta: src/main.rs
+
+/Users/admin/Documents/rust/ownership/ownership/target/debug/deps/ownership-171dfc0a759e0ca6.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/ownership-eb6431489a33d57a.d
@@ -0,0 +1,5 @@
+/Users/admin/Documents/rust/ownership/ownership/target/debug/deps/libownership-eb6431489a33d57a.rmeta: src/main.rs
+
+/Users/admin/Documents/rust/ownership/ownership/target/debug/deps/ownership-eb6431489a33d57a.d: src/main.rs
+
+src/main.rs:
