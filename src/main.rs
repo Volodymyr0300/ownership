d9468c5ecb2203ca7a3365ba04ceaa9be6ace0ba
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-04-19 15:15:10 -0500

    slices_at_ownership

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
 fn main() {
-    let _reference_to_nothing = no_dangle();
-    println!("{}", _reference_to_nothing)
+    // let x = String::from("hello world");
+    // first_word(&x);
+    // println!("{}", first_word(&x));
+
+    let mut s = String::from("hello world");
+    
+    let word = first_word(&s); // word will get the value 5
+    
+    s.clear(); // this empties the String, making it equal to ""
+    // word still has the value 5 here, but there's no more string that
+    // we could meaningfully use the value 5 with. word is now totally
+    // invalid!
 }
     
-/*
-fn dangle() -> &String { // dangle returns a reference to a String
-    let s = String::from("hello"); // s is a new String
-    &s // we return a reference to the String, s
-} // Here, s goes out of scope, and is dropped. Its memory goes away.
-// Danger!
-*/
-
-fn no_dangle() -> String {
-    let s = String::from("hello");
-    s
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+    // println!("{:?}", bytes);
+    // println!("{}", s.as_bytes().len());
+    
+    for (i, &item) in bytes.iter().enumerate() {
+        // println!("Index: {}, Value: {}", i, item);
+        if item == b' ' {
+            return i;
+        }
+    }
+    
+    s.len()
 }
