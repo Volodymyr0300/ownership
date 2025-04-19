3b79945bd9e590fbc432ac1eab449c63382ec0c3
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-04-18 22:23:42 -0500

    mutable_references

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,48 @@
-fn main() {
-    let s1 = String::from("hello");
-    let (s2, len) = calculate_length(s1);
-    println!("The length of '{}' is {}.", s2, len);
-}
+// fn main() {
+//     let s1 = String::from("hello");
+//     let len = calculate_length(&s1);
+//     println!("The length of '{}' is {}.", s1, len);
+    
+//     fn calculate_length(s: &String) -> usize { // s is a reference to a String
+//         s.len()
+//     }
+//     // Here, s goes out of scope. But because it does not have ownership of
+//     // what it refers to, nothing happens.
+// }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len(); // len() returns the length of a String
-    (s, length)
+
+// fn main() {
+//     let s = String::from("hello");
+//     change(&s);
+    
+//     fn change(some_string: &String) {
+//     some_string.push_str(", world");
+//     }
+// }
+
+fn main() {
+    let mut s = String::from("hello");
+    change(&mut s);
+    
+    fn change(some_string: &mut String) {
+        some_string.push_str(", world");
+    }
+
+    /* 
+    let mut s = String::from("hello");
+    {
+    let r1 = &mut s;
+    }
+   
+     // r1 goes out of scope here, so we can make a new reference with no
+    // problems.
+    let r2 = &mut s;
+    println!("{}", r2);
+    */
+
+    let mut s = String::from("hello");
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    let r3 = &mut s; // BIG PROBLEM
+    println!("{}, {}, and {}", r1, r2, r3);
 }
