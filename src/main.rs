f76fd5e028bfc06365abc9f2443bf8602c90069c
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-04-19 16:15:40 -0500

    ownership_other_slices

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,6 @@
 fn main() {
-
-    /*
-    let s = String::from("hello world");
-    let hello = &s[0..5];
-    let world = &s[6..11];
-    println!("{} {}", hello, world);
-     */
-
-    /* 
-    let s = String::from("hello");
-    let slice = &s[0..2];
-    let slice = &s[..2];
-    println!("slice: {}", slice);
-    */
-
-    /* 
-    let s = String::from("hello");
-    let len = s.len();
-    let slice = &s[3..len];
-    let slice = &s[3..];
-    println!("len: {}, slice: {}", len, slice);
-    */
-
-    /* 
-    let s = String::from("hello");
-    let len = s.len();
-    let slice = &s[0..len];
-    let slice = &s[..];
-    println!("len: {}, slice: {}", len, slice);
-    */
-
+    let a = [1, 2, 3, 4, 5];
     
-    let mut s = String::from("hello world");
-    let word = first_word(&s);
-    s.clear(); // error!
-    println!("the first word is: {}", word);
-    
-    
-    fn first_word(s: &String) -> &str {
-        let bytes = s.as_bytes();
-
-        for (i, &item) in bytes.iter().enumerate() {
-
-            if item == b' ' {
-                return &s[0..i];
-            }
-        }
-
-        &s[..]
-    }
-
-    let s = String::from("hello world");
-    let word = first_word(&s);
-    println!("first word: {}", word);
+    let slice = &a[1..3];
+    println!("slice: {:?}", slice);
 }
