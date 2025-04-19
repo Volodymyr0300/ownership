bcd855d287437683a4c270e90aa4259040fec05f
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-04-19 15:40:00 -0500

    ownership_string_slices

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,30 @@
 fn main() {
-    // let x = String::from("hello world");
-    // first_word(&x);
-    // println!("{}", first_word(&x));
 
-    let mut s = String::from("hello world");
-    
-    let word = first_word(&s); // word will get the value 5
-    
-    s.clear(); // this empties the String, making it equal to ""
-    // word still has the value 5 here, but there's no more string that
-    // we could meaningfully use the value 5 with. word is now totally
-    // invalid!
-}
-    
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
-    // println!("{:?}", bytes);
-    // println!("{}", s.as_bytes().len());
-    
-    for (i, &item) in bytes.iter().enumerate() {
-        // println!("Index: {}, Value: {}", i, item);
-        if item == b' ' {
-            return i;
-        }
-    }
-    
-    s.len()
+    /*
+    let s = String::from("hello world");
+    let hello = &s[0..5];
+    let world = &s[6..11];
+    println!("{} {}", hello, world);
+     */
+
+    /* 
+    let s = String::from("hello");
+    let slice = &s[0..2];
+    let slice = &s[..2];
+    println!("slice: {}", slice);
+    */
+
+    /* 
+    let s = String::from("hello");
+    let len = s.len();
+    let slice = &s[3..len];
+    let slice = &s[3..];
+    println!("len: {}, slice: {}", len, slice);
+    */
+
+    let s = String::from("hello");
+    let len = s.len();
+    let slice = &s[0..len];
+    let slice = &s[..];
+    println!("len: {}, slice: {}", len, slice);
 }
