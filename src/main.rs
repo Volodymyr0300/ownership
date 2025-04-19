d4a684fd930758fe198bd4f76445c05756b318fe
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-04-19 09:09:19 -0500

    dangling_references

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,17 @@
-// fn main() {
-//     let s1 = String::from("hello");
-//     let len = calculate_length(&s1);
-//     println!("The length of '{}' is {}.", s1, len);
-    
-//     fn calculate_length(s: &String) -> usize { // s is a reference to a String
-//         s.len()
-//     }
-//     // Here, s goes out of scope. But because it does not have ownership of
-//     // what it refers to, nothing happens.
-// }
-
-
-// fn main() {
-//     let s = String::from("hello");
-//     change(&s);
-    
-//     fn change(some_string: &String) {
-//     some_string.push_str(", world");
-//     }
-// }
-
 fn main() {
-    let mut s = String::from("hello");
-    change(&mut s);
+    let _reference_to_nothing = no_dangle();
+    println!("{}", _reference_to_nothing)
+}
     
-    fn change(some_string: &mut String) {
-        some_string.push_str(", world");
-    }
+/*
+fn dangle() -> &String { // dangle returns a reference to a String
+    let s = String::from("hello"); // s is a new String
+    &s // we return a reference to the String, s
+} // Here, s goes out of scope, and is dropped. Its memory goes away.
+// Danger!
+*/
 
-    /* 
-    let mut s = String::from("hello");
-    {
-    let r1 = &mut s;
-    }
-   
-     // r1 goes out of scope here, so we can make a new reference with no
-    // problems.
-    let r2 = &mut s;
-    println!("{}", r2);
-    */
-
-    let mut s = String::from("hello");
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    let r3 = &mut s; // BIG PROBLEM
-    println!("{}, {}, and {}", r1, r2, r3);
+fn no_dangle() -> String {
+    let s = String::from("hello");
+    s
 }
